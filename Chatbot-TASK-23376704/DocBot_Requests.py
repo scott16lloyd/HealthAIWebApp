0bad59e447dde69f5e2ffab69a9c85ff3bc2c382
Sam Roche <Sam0015@rocketmail.com>
2023-10-09 18:10:29 +0100

    Revert "Added Start/Post/Exit endpoints to the DocBot and DocBot Requests & cleaned code"
    This reverts commit 1ac2c4642bbc579b74bf713a69faaf47ea14b875.
    
    Added to wrong branch

--- Chatbot-TASK-23376704/DocBot_Requests.py
+++ Chatbot-TASK-23376704/DocBot_Requests.py
@@ -1,61 +1,27 @@
-#This is a file to post request and recieve the response to the DocBot API
-
 import requests
 
-
-
-# API endpoint URLs
-base_url = 'http://127.0.0.1:5000' # Defining the base URL for the Flask API
-
-# Flask Endpoints
-start_url = f'{base_url}/start' #Start Endpoint
-post_url = f'{base_url}/post' #Post Endpoint
-exit_url = f'{base_url}/exit' #Exit Endpoint
-
-
-#Function to start the conversation
-def start_conversation():
-    response = requests.get(start_url)
-    return response.json()["message"]
-
-
+# API endpoint URL
+api_url = 'http://127.0.0.1:5000/diagnose'
 
 def send_message(message):
-    try:
-
-        # Send a post request to the chatbot
-        response = requests.post(post_url, json={"user_input": message})
-        return response.json()["response"]
-    except Exception as e:
-        return str(e)
-
-def exit_chat():
-
-    # Exiting the chat
-    response = requests.get(exit_url)
-    return response.json()["message"]
-
-
+    # Send a POST request to the API
+    response = requests.post(api_url, json={"user_input": message})
+    
+    # Return the response from the API
+    return response.json()["response"]
 
 if __name__ == "__main__":
-  
-    
-
-    # Start the conversation
-    start_message = start_conversation()
-    print(start_message)
-
+    # Send on start up
+    print("Welcome to the Medical Chatbot!")
+    print("You can start the conversation. Type 'exit' to quit.")
 
     while True:
+        # Formatting conversation
         user_input = input("You: ")
         
+        # Ending the conversation
         if user_input.lower() == "exit":
-            # Exit the chat
-            exit_message = exit_chat()
-            print(exit_message)
             break
 
-
-        #Getting bot response
         bot_response = send_message(user_input)
-        print("Doc-bot:", bot_response)
+        print(bot_response)
