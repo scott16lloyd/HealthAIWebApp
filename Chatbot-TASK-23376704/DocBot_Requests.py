1ac2c4642bbc579b74bf713a69faaf47ea14b875
Sam Roche <Sam0015@rocketmail.com>
2023-10-09 18:07:58 +0100

    Added Start/Post/Exit endpoints to the DocBot and DocBot Requests & cleaned code

--- Chatbot-TASK-23376704/DocBot_Requests.py
+++ Chatbot-TASK-23376704/DocBot_Requests.py
@@ -1,27 +1,61 @@
+#This is a file to post request and recieve the response to the DocBot API
+
 import requests
 
-# API endpoint URL
-api_url = 'http://127.0.0.1:5000/diagnose'
+
+
+# API endpoint URLs
+base_url = 'http://127.0.0.1:5000' # Defining the base URL for the Flask API
+
+# Flask Endpoints
+start_url = f'{base_url}/start' #Start Endpoint
+post_url = f'{base_url}/post' #Post Endpoint
+exit_url = f'{base_url}/exit' #Exit Endpoint
+
+
+#Function to start the conversation
+def start_conversation():
+    response = requests.get(start_url)
+    return response.json()["message"]
+
+
 
 def send_message(message):
-    # Send a POST request to the API
-    response = requests.post(api_url, json={"user_input": message})
-    
-    # Return the response from the API
-    return response.json()["response"]
+    try:
+
+        # Send a post request to the chatbot
+        response = requests.post(post_url, json={"user_input": message})
+        return response.json()["response"]
+    except Exception as e:
+        return str(e)
+
+def exit_chat():
+
+    # Exiting the chat
+    response = requests.get(exit_url)
+    return response.json()["message"]
+
+
 
 if __name__ == "__main__":
-    # Send on start up
-    print("Welcome to the Medical Chatbot!")
-    print("You can start the conversation. Type 'exit' to quit.")
+  
+    
+
+    # Start the conversation
+    start_message = start_conversation()
+    print(start_message)
+
 
     while True:
-        # Formatting conversation
         user_input = input("You: ")
         
-        # Ending the conversation
         if user_input.lower() == "exit":
+            # Exit the chat
+            exit_message = exit_chat()
+            print(exit_message)
             break
 
+
+        #Getting bot response
         bot_response = send_message(user_input)
-        print(bot_response)
+        print("Doc-bot:", bot_response)
