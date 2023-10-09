d55a9d9ecb0980524c5647bc579bef1d8c21f491
scott lloyd <scottlloyd@scotts-MacBook-Pro.local>
2023-10-09 16:04:00 +0100

    Finished search bar component, refactored Patient Overview Widget default avatar

--- src/components/widgets/PatientOverviewWidget/PatientOverviewWidget.jsx
+++ src/components/widgets/PatientOverviewWidget/PatientOverviewWidget.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 import { Card, Avatar, Typography } from '@mui/material';
 
-function PatientOverviewWidget({ name, id, avatar }) {
+function stringToColor(string) {
+  if (string) {
+    let hash = 0;
+    let i;
+
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+
+    return color;
+  } else {
+    let color = '#D9D9D9';
+    return color;
+  }
+}
+
+function stringAvatar(name) {
+  if (name !== '') {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    };
+  } else {
+    return 'n/a';
+  }
+}
+
+function PatientOverviewWidget({ name, id }) {
   const avatarContainerStyles = {
     display: 'flex',
     justifyContent: 'center',
@@ -32,10 +68,10 @@ function PatientOverviewWidget({ name, id, avatar }) {
     >
       <div style={avatarContainerStyles}>
         <Avatar
+          style={{ height: '80px', width: '80px', fontSize: '2rem' }}
           variant="outlined"
-          sx={{ width: 80, height: 80 }}
           alt="Default Profile Image"
-          src={avatar}
+          {...stringAvatar(name)}
         />
       </div>
       <div style={textContainerStyles}>
--- src/components/widgets/SearchBar/SearchBar.jsx
+++ src/components/widgets/SearchBar/SearchBar.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
-  const inputStyle = {
-    boxShadow: 'none',
-  };
+  const options = ['Option 1', 'Option 2', 'Option 3'];
 
   return (
-    <TextField
-      variant="outlined"
-      placeholder="Search..."
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-        style: inputStyle,
-      }}
-      fullWidth
+    <Autocomplete
+      freeSolo
+      options={options}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="outlined"
+          placeholder="Search..."
+          fullWidth
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            sx: {
+              '& .MuiInputBase-input': { boxShadow: 'none' },
+            },
+          }}
+        />
+      )}
     />
   );
 };
