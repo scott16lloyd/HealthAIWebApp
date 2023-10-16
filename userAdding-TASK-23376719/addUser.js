053dba7a221081df16929913cec2f1bb4d77b5f4
scott16lloyd <111532400+scott16lloyd@users.noreply.github.com>
2023-10-16 20:02:03 +0100

    fix: removed firebaseConfig details in userAdding

--- userAdding-TASK-23376719/addUser.js
+++ userAdding-TASK-23376719/addUser.js
@@ -1,14 +1,4 @@
 import { firebaseConfig } from "../src/firebase";
-//const firebaseConfig = {
-//    apiKey: "AIzaSyAHFVjJjJeM_Soz2lsJOEIwSBiLWOS_RY0",
-// authDomain: "healthai-40b47.firebaseapp.com",
-//  databaseURL: "https://healthai-40b47-default-rtdb.europe-west1.firebasedatabase.app",
-//  projectId: "healthai-40b47",
-//  storageBucket: "healthai-40b47.appspot.com",
-//  messagingSenderId: "493505764604",
-//  appId: "1:493505764604:web:2decb83a82f453f0398b79",
-//  measurementId: "G-YDS75RSZME"
-//};
 
 //to initilize firebase
 firebase.initializeApp(firebaseConfig);
