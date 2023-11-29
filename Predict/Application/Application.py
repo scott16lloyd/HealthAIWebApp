d748852eb4b5d1c7d6059a5ba2759a993c09835d
Sam Roche <Sam.roche@mycit.ie>
2023-11-29 09:29:25 +0000

    WIP: Predict working with given UUID - Fixed Database

--- Predict/Application/Application.py
+++ Predict/Application/Application.py
@@ -1,6 +1,13 @@
+import os
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
+import firebase_admin
+from firebase_admin import credentials, db
+import random
+
+random_seed = 42  
+random.seed(random_seed)
+np.random.seed(random_seed)
 
 import Colon
 import Heart
@@ -8,130 +15,171 @@ import Lung
 
 GENDER_MAPPING = {"M": 0, "F": 1}
 
+#Get the current directory of the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Function to generate random patient data
-# Function to generate random patient data
-def generate_single_patient_data(model_type):
-    age = np.random.randint(30, 80)
-    sex = np.random.choice(['M', 'F'])
-    alcohol = np.random.randint(0, 2)
-    bowel_problems = np.random.randint(0, 2)
-    diabetic = np.random.randint(0, 2)
-    rectal_bleeding = np.random.randint(0, 2)
-    smoking = np.random.randint(0, 2)
-    stomach_cramps = np.random.randint(0, 2)
-    tiredness = np.random.randint(0, 2)
-    weight_loss = np.random.randint(0, 2)
+#Construct the path to the service account key file
+path = os.path.join(script_dir, "healthai-40b47-firebase-adminsdk-6kz46-81ac2aa293.json")
 
-    bp = np.random.randint(80, 200)
-    ChestPain = np.random.randint(0, 2)
-    Cholestrol = np.random.randint(100, 300)
-    Max_HR = np.random.randint(60, 220)
+#Use the constructed path for Firebase initialization
+cred = credentials.Certificate(path)
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://healthai-40b47-default-rtdb.europe-west1.firebasedatabase.app"}
+    )
 
-    chronic_Disease = np.random.randint(0, 2)
-    coughing = np.random.randint(0, 2)
-    fatigue = np.random.randint(0, 2)
-    shortness_of_breath = np.random.randint(0,2)
-    swallowing_difficulty = np.random.randint(0, 2)
-    wheezing = np.random.randint(0, 2)
-    yellow_fingers = np.random.randint(0, 2)
+def fetch_patient_data(patient_id):
+    try:
+        #Get the patient entity
+        ref = db.reference(f'patients/{patient_id}')
+        patient_data = ref.get()
 
-    patient_data_colon = {
-        'Age': age,
-        'Sex': GENDER_MAPPING[sex],
-        'Alcohol': alcohol,
-        'Bowel Problems': bowel_problems,
-        'Diabetic': diabetic,
-        'Rectal Bleeding': rectal_bleeding,
-        'Smoking': smoking,
-        'Stomach Cramps': stomach_cramps,
-        'Tiredness': tiredness,
-        'Weight Loss': weight_loss,
-    }
+        if patient_data:
+            print("Patient Data Fetched:")
+            print(patient_data)
+            medical_records = patient_data.get("medicalRecords", [])
+            if medical_records:
+                latest_submission = max(medical_records, key=lambda x: x.get("timestamp", 0))
+                return latest_submission
+            else:
+                print(f"Debug: No medical records found for patient ID {patient_id}")
+                return None
+        else:
+            print(f"Debug: Patient Entity does not exist for ID {patient_id}")
+            return None
+    except Exception as e:
+        print(f"Error fetching patient data: {e}")
+        return None
 
-    patient_data_heart = {
-        'Age': age,
-        'Sex': GENDER_MAPPING[sex], 
-        'BP': bp,
-        'Chest pain type': ChestPain,
-        'Cholesterol': Cholestrol,
-        'Max HR': Max_HR,
-    }
+def generate_single_patient_data(model_type, user_uuid):
+    #Fetch user data from patients table using UUID
+    user_ref = db.reference(f'patients/{user_uuid}')
+    user_data = user_ref.get()
 
-    patient_data_lung = {
-        'AGE': age,
-        'GENDER': GENDER_MAPPING[sex],  
-        'ALCOHOL CONSUMING': alcohol,
-        'CHEST PAIN': ChestPain,
-        'CHRONIC DISEASE': chronic_Disease,
-        'COUGHING': coughing,
-        'FATIGUE': fatigue,
-        'SHORTNESS OF BREATH': shortness_of_breath,
-        'SMOKING': smoking,
-        'SWALLOWING DIFFICULTY': swallowing_difficulty,
-        'WHEEZING': wheezing,
-        'YELLOW_FINGERS': yellow_fingers,
-    }
+    if not user_data:
+        raise ValueError(f"Patient not found for UUID: {user_uuid}")
 
+    #Extract patient attributes
+    age = user_data.get("Age")
+    sex = user_data.get("gender")
+    
+    #Extract medical records from the list
+    medical_records = user_data.get("medicalRecords", [])
 
+    if not medical_records:
+        raise ValueError(f"No medical records found for UUID: {user_uuid}")
 
+    #Get the medical record with the highest index ID
+    latest_record = max(medical_records, key=lambda x: x.get("id", 0))
+
+    #Extract values from the latest medical record
+    alcohol = latest_record.get("Q2", 0)
+    bowel_problems = latest_record.get("Q16", 0)
+    diabetic = latest_record.get("Q4", 0)
+    rectal_bleeding = latest_record.get("Q17", 0)
+    smoking = latest_record.get("Q1", 0)
+    stomach_cramps = latest_record.get("Q11", 0)
+    tiredness = latest_record.get("Q5", 0)
+    weight_loss = latest_record.get("Q6", 0)
+
+    bp = latest_record.get("Q7", 0)
+    chest_pain = latest_record.get("Q10", 0)
+    cholesterol = latest_record.get("Q9", 0)
+    max_hr = latest_record.get("Q8", 0)
+
+    chronic_disease = latest_record.get("Q3", 0)
+    coughing = latest_record.get("Q14", 0)
+    fatigue_lung = latest_record.get("Q5", 0)
+    shortness_of_breath = latest_record.get("Q15", 0)
+    swallowing_difficulty = latest_record.get("Q12", 0)
+    wheezing = latest_record.get("Q13", 0)
+    yellow_fingers = latest_record.get("Q12", 0)
+
+    #Create patient data based on model type
     if model_type == 'colon':
-        return patient_data_colon
+        return {
+            'Age': age,
+            'Sex': GENDER_MAPPING.get(sex, 0),
+            'Alcohol': alcohol,
+            'Bowel Problems': bowel_problems,
+            'Diabetic': diabetic,
+            'Rectal Bleeding': rectal_bleeding,
+            'Smoking': smoking,
+            'Stomach Cramps': stomach_cramps,
+            'Tiredness': tiredness,
+            'Weight Loss': weight_loss,
+        }
     elif model_type == 'heart':
-        return patient_data_heart
+        return {
+            'Age': age,
+            'Sex': GENDER_MAPPING.get(sex, 0),
+            'BP': bp,
+            'Chest pain type': chest_pain,
+            'Cholesterol': cholesterol,
+            'Max HR': max_hr,
+        }
+    
     elif model_type == 'lung':
-        return patient_data_lung
+        return {
+            'AGE': age,
+            'GENDER': GENDER_MAPPING.get(sex, 0),
+            'ALCOHOL CONSUMING': alcohol,
+            'CHEST PAIN': chest_pain,
+            'CHRONIC DISEASE': chronic_disease,
+            'COUGHING': coughing,
+            'FATIGUE': fatigue_lung,
+            'SHORTNESS OF BREATH': shortness_of_breath,
+            'SWALLOWING DIFFICULTY': swallowing_difficulty,
+            'SMOKING': smoking,
+            'WHEEZING': wheezing,
+            'YELLOW_FINGERS': yellow_fingers,
+        }
     else:
         raise ValueError(f"Invalid model_type: {model_type}")
 
-# Example usage:
-single_patient_data_colon = generate_single_patient_data('colon')
-single_patient_data_heart = generate_single_patient_data('heart')
-single_patient_data_lung = generate_single_patient_data('lung')
-
+    
+    
 if __name__ == "__main__":
-    # Generate patient data for colon model
-    single_patient_data_colon = generate_single_patient_data('colon')
+    user_uuid = "al5gO6mkgdZWlR9XqtdMInLHwvl2"
 
-    # Train the colon cancer model
+    #Generate patient data for colon model
+    single_patient_data_colon = generate_single_patient_data('colon', user_uuid)
+
+    #Train the colon cancer model
     colon_model, colon_imputer, train_columns_colon = Colon.train_colon_cancer_model()
 
     print("Input Data for Colon Prediction:")
     print(pd.DataFrame([single_patient_data_colon]))
 
-    # Predict colon cancer
+    #Predict colon cancer
     colon_result, colon_probability = Colon.predict_colon_cancer(
         pd.DataFrame([single_patient_data_colon]), colon_model, colon_imputer, GENDER_MAPPING, train_columns_colon
     )
 
-    # Print results with risk percentage
+    #Print results with risk percentage
     print("\nColon Cancer Data:")
     print("Colon Result (Probability of Positive Class):", colon_result[0])
-    print("Colon Risk Percentage:", colon_probability[0]*100)
+    print("Colon Risk Percentage:", int(colon_probability[0] * 100))
 
-    # Generate patient data for heart model
-    single_patient_data_heart = generate_single_patient_data('heart')
+    #Generate patient data for heart model
+    single_patient_data_heart = generate_single_patient_data('heart', user_uuid)
 
-    # Train the heart disease model
+    #Train the heart disease model
     heart_model, heart_imputer, train_columns_heart = Heart.train_heart_disease_model()
 
     print("\nInput Data for Heart Disease Prediction:")
     print(pd.DataFrame([single_patient_data_heart]))
 
-    # Predict heart disease
+    #Predict heart disease
     heart_result, heart_probability = Heart.predict_heart_disease(
         pd.DataFrame([single_patient_data_heart]), heart_model, heart_imputer, GENDER_MAPPING, train_columns_heart
     )
 
-    # Print results with risk percentage
+    #Print results with risk percentage
     print("\nHeart Disease Data:")
-    #print("Heart Result (Probability of Positive Class):", heart_result[0])
-    print("Heart Risk Percentage:", heart_probability[0]*100)
+    print("Heart Risk Percentage:", int(heart_probability[0] * 100))
 
-     
-    # Generate patient data for lung model
-    single_patient_data_lung = generate_single_patient_data('lung')
-
+    #Generate patient data for lung model
+    single_patient_data_lung = generate_single_patient_data('lung', user_uuid)
 
     #Train the lung cancer model
     lung_model, lung_imputer, train_columns_lung = Lung.train_lung_cancer_model()
@@ -139,14 +187,12 @@ if __name__ == "__main__":
     print("\nInput Data for Lung Prediction:")
     print(pd.DataFrame([single_patient_data_lung]))
 
-    # Predict lung cancer
-lung_result, lung_probability = Lung.predict_lung_cancer(
-    pd.DataFrame([single_patient_data_lung]), lung_model, lung_imputer, GENDER_MAPPING
-)
+    #Predict lung cancer
+    lung_result, lung_probability = Lung.predict_lung_cancer(
+        pd.DataFrame([single_patient_data_lung]), lung_model, lung_imputer, GENDER_MAPPING
+    )
 
-# Print results with risk percentage
-print("\nLung Cancer Data:")
-print("Lung Result (Probability of Positive Class):", lung_result[0])
-
-
-print("Lung Risk Percentage:", (lung_probability[0]) * 100)
+    #Print results with risk percentage
+    print("\nLung Cancer Data:")
+    print("Lung Result (Probability of Positive Class):", lung_result[0])
+    print("Lung Risk Percentage:", int(lung_probability[0] * 100))
